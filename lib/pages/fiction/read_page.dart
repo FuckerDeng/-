9e7845ef75a2970155ccd16658cffe53e0a8fe14
dengfan <844537819@qq.com>
2019-08-16 16:48:06 +0800

    翻页动画还未完

--- lib/pages/fiction/read_page.dart
+++ lib/pages/fiction/read_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter_easyrefresh/easy_refresh.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter/services.dart';
 import '../../public/my_theme.dart';
+import 'dart:ui';
+import 'dart:async';
 
 
 
@@ -25,6 +27,8 @@ class _ReadPage extends State<ReadPage> {
   int nowIndex = 0;
   double dragEnd = 0;
   bool changed = false;
+  bool leftChanged = false;
+  double dragDownDx = 0;
   @override
   void initState() {
     // TODO: implement initState
@@ -44,7 +48,7 @@ class _ReadPage extends State<ReadPage> {
     SystemChrome.setSystemUIOverlayStyle(MyTheme.light);
     double statusTopBarHeight = MediaQuery.of(context).padding.top;
     double statusBottomBarHeight = MediaQuery.of(context).padding.bottom;
-    print("build执行了");
+//    print("build执行了");
     // TODO: implement build
     return new Scaffold(
       backgroundColor: Colors.white,
@@ -57,20 +61,73 @@ class _ReadPage extends State<ReadPage> {
                 border: Border.all(color: Colors.black)
             ),
             child:new Stack(
-              children: <Widget>[
-                readPage2(1,Colors.red)
-              ]
+              children: getPages()
             )
         ),
+        onHorizontalDragDown: (DragDownDetails details){
+          print("下按的坐标：${details.globalPosition.dx}");
+          dragDownDx = details.globalPosition.dx;
+        },
         onHorizontalDragUpdate: (DragUpdateDetails details){
+          double nowDx = details.globalPosition.dx;
+          double moviedLen = nowDx-dragDownDx;
+//          print(nowPage);
           setState(() {
-            pageLeft.update(nowPage, (value)=>(value+details.delta.dx));
             changed = true;
-            print("滑动了，dragEnd:${pageLeft[nowPage]}");
+            if((moviedLen)<0){
+              pageLeft.update(nowPage, (value)=>(moviedLen));
+              print("当前页左滑动了: ${pageLeft[nowPage]}");
+            }else{
+              if(nowPage==1){
+                print("已是第一页，尝试向右滑动");
+                return;
+              }
+              pageLeft.update(nowPage-1, (value)=>(pageLeft[nowPage-1]-moviedLen));
+              leftChanged  = true;
+            }
+
           });
         },
         onHorizontalDragEnd: (DragEndDetails details){
-          print("结束");
+          print("当前页码：${nowPage}");
+          print(pageLeft[nowPage-1]);
+          bool left = pageLeft[nowPage-1]!=null?pageLeft[nowPage-1].abs()<=200:true;
+          if(nowPage == 1){
+            if(pageLeft[nowPage]<0 && left){
+//              print("执行了");
+              if(pageLeft[nowPage].abs()>=50){//屏幕的1/4，就翻页
+                print("滑动大于50进行翻页");
+                print(pageLeft[nowPage]);
+                print(pageLeft[nowPage].abs());
+                while(pageLeft[nowPage].abs()<180){//屏幕的宽度
+                  setState(() {
+                    changed = true;
+                    pageLeft.update(nowPage, (value)=>(pageLeft[nowPage]-2));
+                    print("继续左移一点");
+                  });
+                  Future.delayed(new Duration(milliseconds: 35));
+                }
+                setState(() {
+                  pageLeft.update(nowPage, (value)=>(-180));
+                });
+                print(pageLeft[nowPage].abs());
+                nowPage +=1;
+                return;
+              }
+              print("滑动小于50不翻页");
+              while(pageLeft[nowPage]<0){
+                setState(() {
+                  changed = true;
+                  pageLeft.update(nowPage, (value)=>(pageLeft[nowPage]+2));
+                });
+              }
+              Future.delayed(new Duration(milliseconds: 2));
+              pageLeft.update(nowPage, (value)=>(0));
+            }
+          }else{
+
+          }
+
         },
       )
     );
@@ -78,24 +135,54 @@ class _ReadPage extends State<ReadPage> {
 
   List<Widget> getPages(){
     if(changed){
-      print("改变了");
-      pages.removeAt(nowPage-1);
-      pages.insert(nowPage-1, readPage2(nowPage, Colors.red));
+      if(leftChanged){
+        print("左页改变了");
+        pages.removeAt(nowPage-2);
+        pages.insert(nowPage-2, readPage2(nowPage, Colors.orange));
+        leftChanged = false;
+      }else{
+        print("当前页改变了");
+        pages.removeAt(nowPage-1);
+        pages.insert(nowPage-1, readPage2(nowPage, Colors.pink));
+      }
       changed = false;
     }
     return pages.reversed.toList();
   }
 
+  checkNums(){
+    TextPainter();
+  }
 
   Widget readPage2(int num,Color color){
     return new Positioned(
       top: 0,
-      left: pageLeft[num-1],
+      left: pageLeft[num],
       child: Container(
+        decoration: new BoxDecoration(
+          color: color,
+          boxShadow:[BoxShadow(color: Colors.grey, offset: Offset(0.5, 0.5), blurRadius: 0, spreadRadius: 2.0), BoxShadow(color: Color(0x9900FF00), offset: Offset(1.0, 1.0)), BoxShadow(color: Colors.grey)],
+        ),
         width: 200+num*10.0,
         height: 200+num*10.0,
-        color: color,
-        child: new Text("${num}"),
+
+        child: new Column(
+          children: <Widget>[
+            new Text("1"),
+            new Text("2"),
+            new Text("3"),
+            new Text("4"),
+            new Text("5"),
+            new Text("6"),
+            new Text("7"),
+            new Text("8"),
+            new Text("9"),
+            new Text("10"),
+            new Text("11"),
+            new Text("12"),
+            new Text("12"),
+          ],
+        )
       ),
     );
   }
